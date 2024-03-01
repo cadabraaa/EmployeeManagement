9b32935181f26da53fb956ac84d7302c3861ab0e
cadabraaa <151050186+cadabraaa@users.noreply.github.com>
2024-03-01 02:55:19 +0000

    login done

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,41 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 import os
 
 db = SQLAlchemy()
 
+
 def create_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  db_connection = os.environ['DB_CONNECTION_STRING']
-  app.config['SQLALCHEMY_DATABASE_URI'] = db_connection
+    app.config['SECRET_KEY'] = 'secret_key'
+    db_connection = os.environ['DB_CONNECTION_STRING']
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_connection
   
-  db.init_app(app)
+    db.init_app(app)
 
-  from .models import User
+    from .views import views
+    from .auth import auth
 
-  with app.app_context():
-    db.create_all()
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-  @app.route('/')
-  def index():
-    return 'Hello from Flask!'
+    from .models import User, Note
 
-  return app
-  
+    with app.app_context():
+      db.create_all()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+      return User.query.get(int(id))
+
+    return app
 
 
   
--- website/models.py
+++ website/models.py
@@ -1,12 +1,60 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+
 
 class User(db.Model, UserMixin):
-  id = db.Column(db.Integer, primary_key=True)
-  first_name = db.Column(db.String(150))
-  middle_name = db.Column(db.String(150))
-  first_name = db.Column(db.String(150))
-  ro = db.Column(db.String(150))
-  email = db.Column(db.String(150), unique=True)
-  password = db.Column(db.String(150))
-  notes = db.relationship('Note')
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
+
+'''
+class Superadmin(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    company = db.relationship('Company')
+
+class Company(db.Model, UserMixin):
+    company_id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(150), db.ForeignKey('role.role_type'))
+    company_name = db.Column(db.String(150))
+    establishment_date = db.Column(db.Date)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    mobile = db.Column(db.Integer)
+    superadmin_id = db.Column(db.Integer, db.ForeignKey('superadmin.id'))
+    employee = db.relationship('Employee')
+    role = db.relationship('Role')
+
+class Employee(db.Model, UserMixin):
+    employee_id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(150), db.ForeignKey('role.role_type'))
+    first_name = db.Column(db.String(150))
+    middle_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    mobile = db.Column(db.Integer)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.company_id'))
+    role = db.relationship('Role')
+
+class Role(db.Model, UserMixin):
+    role_id = db.Column(db.Integer, primary_key=True)
+    role_type = db.Column(db.String(150), unique=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.company_id'))
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.employee_id'))
+
+'''
