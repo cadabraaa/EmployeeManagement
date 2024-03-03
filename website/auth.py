14869cc8f4a258586c7dd4de426507e7fce64218
cadabraaa <151050186+cadabraaa@users.noreply.github.com>
2024-03-03 06:59:58 +0000

    login

--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,12 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import random
+import string
 
 auth= Blueprint('auth', __name__)
+
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,12 +36,16 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-
-@auth.route('/sign-up', methods=['GET', 'POST'] )
+    
+'''
+@auth.route('/sign_up', methods=['GET', 'POST'] )
 def sign_up():
   if request.method == 'POST':
+    company_code = request.form.get('company_code')
+    company_name = request.form.get('company_name')
+    establishment_year = request.form.get('establishment_year')
+    mobile = request.form.get('mobile')
     email = request.form.get('email')
-    first_name = request.form.get('firstName')
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
@@ -47,14 +55,12 @@ def sign_up():
         flash('Email already exists.', category='error')
     elif len(email) < 4:
         flash('Email must be greater than 3 characters.', category='error')
-    elif len(first_name) < 2:
-        flash('First name must be greater than 1 character.', category='error')
     elif password1 != password2:
         flash('Passwords don\'t match.', category='error')
     elif len(password1) < 7:
         flash('Password must be at least 7 characters.', category='error')
     else:
-        new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+        new_user = User(email=email, company_code=company_code, company_name=company_name, mobile=mobile, password=generate_password_hash(
             password1, method='pbkdf2:sha256'))
         db.session.add(new_user)
         db.session.commit()
@@ -63,3 +69,5 @@ def sign_up():
         return redirect(url_for('views.home'))
 
   return render_template("sign_up.html", user=current_user)
+
+'''
--- website/models.py
+++ website/models.py
@@ -5,22 +5,59 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    company_code = db.Column(db.String(3), unique=True, nullable=False)
+    company_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(150), unique=True)
+    mobile = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    establishment_year = db.Column(db.String(4))
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 
 
 '''
+
+class Role(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  role = db.Column(db.String(50), unique=True)
+  description = db.Column(db.String(255))
+
+
+class Employee(db.Model):
+  employee_id = db.Column(db.String(15), primary_key=True)
+  first_name = db.Column(db.String(150))
+  middle_name = db.Column(db.String(150))
+  last_name = db.Column(db.String(150))
+  email = db.Column(db.String(150), unique=True)
+  mobile = db.Column(db.Integer)
+  role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+  
+class Company(db.Model, UserMixin):
+  company_code = db.Column(db.String(3), primary_key=True, nullable=False)
+  company_name = db.Column(db.String(150))
+  establishment_year = db.Column(db.Integer)
+  email = db.Column(db.String(150), unique=True)
+  password = db.Column(db.String(150))
+  mobile = db.Column(db.Integer)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  
+class Company(db.Model, UserMixin):
+  company_id = db.Column(db.Integer, primary_key=True)
+  role = db.Column(db.String(150), db.ForeignKey('role.role_type'))
+  company_name = db.Column(db.String(150))
+  establishment_date = db.Column(db.Date)
+  email = db.Column(db.String(150), unique=True)
+  password = db.Column(db.String(150))
+  mobile = db.Column(db.Integer)
+  superadmin_id = db.Column(db.Integer, db.ForeignKey('superadmin.id'))
+  employee = db.relationship('Employee')
+  role = db.relationship('Role')
+
+  
 class Superadmin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
